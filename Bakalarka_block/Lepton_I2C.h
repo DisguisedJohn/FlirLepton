faced751c4515359d4b7fd19a503a031f3cca9fb
DisguisedJohn <svobodahonza@outlook.com>
2020-04-05 22:36:50 +0200

    Init copy of files

---
+++ Bakalarka_block/Lepton_I2C.h
@@ -0,0 +1,9 @@
+#ifndef LEPTON_I2C
+#define LEPTON_I2C
+
+void lepton_perform_ffc();
+void lepton_reboot();
+void lepton_agcDisable();
+void lepton_gainmode_low();
+
+#endif
---
+++ Bakalarka_block/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Lepton_I2C.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    //lepton_reboot();
+    //lepton_agcDisable();
+    lepton_perform_ffc();
+    //lepton_gainmode_low();
+    return 0;
+}
