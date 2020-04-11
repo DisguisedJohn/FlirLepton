a3aa6a673c47cccb9049bda2c671900a6c977150
DisguisedJohn <svobodahonza@outlook.com>
2020-04-12 01:05:00 +0200

    Generace dat pro prezentaci

--- Bakalarka_block/main.cpp
+++ Bakalarka_block/main.cpp
@@ -8,8 +8,8 @@ int main()
     cout << "Hello world!" << endl;
 
     //lepton_reboot();
-    //lepton_agcDisable();
-    lepton_perform_ffc();
+    lepton_agcDisable();
+    //lepton_perform_ffc();
     //lepton_gainmode_low();
     return 0;
 }
